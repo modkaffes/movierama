32aed49ae3ae9288d66e1fb0fd2b05cf5b39fe36
Modestos Kaffes <modkaffes@gmail.com>
2023-03-20 10:29:03 +0100

    Update Searchbar component
    - UI improvements
    - Fix behavior when query is empty. It ran the query before, whereas
      now it runs only when query exists.

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,61 +1,48 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { searchMovies } from "@/api/movies";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { useSearchMovies } from "@/services/hooks/useMovies";
 import { useDebouncedValue } from "rooks";
 
 function Searchbar() {
-  const [query, setQuery] = useState<string>("");
+  const [query, setQuery] = useState<string>();
   const [debouncedQuery] = useDebouncedValue(query, 250);
 
-  const { data } = useQuery({
-    queryKey: ["movies", "search", debouncedQuery],
-    queryFn: () => debouncedQuery && searchMovies(debouncedQuery),
-  });
-
-  // Focus on search input when pressing ctrl + k
-  useEffect(() => {
-    function handleKeyDown(event: KeyboardEvent) {
-      if (event.ctrlKey && event.key === "k") {
-        event.preventDefault();
-        document.getElementById("search")?.focus();
-      }
-    }
-
-    document.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
+  const { data } = useSearchMovies(debouncedQuery);
 
   return (
     <div className="relative flex min-w-[340px] items-center md:ml-auto">
-      <form role="search" className="block w-full">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <form
+        role="search"
+        className="relative block w-full text-white focus-within:text-gray-400"
+      >
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <MagnifyingGlassIcon
+            className="h-5 w-5 flex-shrink-0"
+            aria-hidden="true"
+          />
+        </div>
         <input
           type="search"
           name="search"
           id="search"
-          aria-label="Search for a movie"
-          className="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
-          placeholder="Search for a movie"
-          value={query}
+          aria-label="Search movies"
+          className="block w-full rounded-md border-0 bg-gray-700 py-1.5 pl-10 pr-3 text-white placeholder:text-white focus:bg-white focus:text-gray-900 focus:ring-0 focus:placeholder:text-gray-400 sm:text-sm sm:leading-6"
+          placeholder="Search movies"
           onChange={(event) => setQuery(event.target.value)}
         />
-        <div className="absolute inset-y-0 right-0 hidden py-1.5 pr-1.5 sm:flex">
-          <kbd className="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400">
-            ^K
-          </kbd>
-        </div>
       </form>
       {debouncedQuery && data?.results.length > 0 && (
-        <ul className="absolute top-full z-50 flex max-h-[600px] w-full flex-col overflow-y-auto rounded-md border border-gray-700 bg-gray-800 text-sm">
+        <ul className="absolute top-full z-50 flex max-h-[600px] w-full flex-col divide-y overflow-y-auto rounded-md border border-gray-700 bg-gray-800 text-sm">
           {data.results?.map((movie: Movie) => (
-            <li key={movie.id}>
+            <li key={movie.id} className="border-gray-900">
               <NavLink
                 to={`/movies/${movie.id}`}
                 onClick={() => setQuery("")}
-                className="flex gap-2 p-1 hover:bg-gray-700 focus:bg-gray-700 "
+                className="flex items-center gap-2 p-1 hover:bg-gray-700 focus:bg-gray-700"
               >
                 <div className="w-10 flex-shrink-0">
                   {movie.poster_path ? (
@@ -67,18 +54,20 @@ function Searchbar() {
                       className="w-full rounded-sm"
                     />
                   ) : (
-                    <div className="h-10 w-10 bg-gray-200" />
+                    <div className="h-14 w-10 rounded-sm bg-gray-600" />
                   )}
                 </div>
-                {movie.title}{" "}
-                {movie.release_date && `(${movie.release_date.slice(0, 4)})`}
+                <span>
+                  {movie.title}{" "}
+                  {movie.release_date && `(${movie.release_date.slice(0, 4)})`}
+                </span>
               </NavLink>
             </li>
           ))}
         </ul>
       )}
       {debouncedQuery && data?.results.length === 0 && (
-        <div className="absolute top-full w-full rounded-md border ">
+        <div className="absolute top-full w-full rounded-md border border-gray-700 p-4 text-sm">
           <p>No movies found with this title</p>
         </div>
       )}
