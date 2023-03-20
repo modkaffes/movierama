541f87db63729b28e79f18fee6578917c1f230f1
Modestos Kaffes <modkaffes@gmail.com>
2023-03-20 10:37:27 +0100

    Lazy load routes components

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import { StrictMode, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "@/App";
-import Favorites from "@/routes/Favorites";
-import Movie from "@/routes/Movie";
-import NotFound from "@/routes/NotFound";
-import Trending from "@/routes/Trending";
-import Watchlist from "@/routes/Watchlist";
 import "@/styles/index.css";
+
+const NotFoundRoute = lazy(async () => import("@/routes/NotFound"));
+const TrendingRoute = lazy(async () => import("@/routes/Trending"));
+const MovieRoute = lazy(async () => import("@/routes/Movie"));
+const FavoritesRoute = lazy(async () => import("@/routes/Favorites"));
+const WatchlistRoute = lazy(async () => import("@/routes/Watchlist"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,20 +26,20 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      { path: "*", element: <NotFound /> },
-      { index: true, element: <Trending /> },
-      { path: "movies/:movieId", element: <Movie /> },
-      { path: "favorites", element: <Favorites /> },
-      { path: "watchlist", element: <Watchlist /> },
+      { path: "*", element: <NotFoundRoute /> },
+      { index: true, element: <TrendingRoute /> },
+      { path: "movies/:movieId", element: <MovieRoute /> },
+      { path: "favorites", element: <FavoritesRoute /> },
+      { path: "watchlist", element: <WatchlistRoute /> },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ReactQueryDevtools />
     </QueryClientProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
